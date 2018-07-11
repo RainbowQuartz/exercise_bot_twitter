0b70ea488154446544b5f0526ea1a5d6f1dfd419
Léona Chevrel <chevrel.leona@gmail.com>
2018-07-11 12:37:29 +0200

    first commit

---
+++ first_bot.rb
@@ -0,0 +1,12 @@
+require 'twitter'
+require 'dotenv'
+require 'pry'
+
+Dotenv.load
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  config.access_token        = ENV["TWITTER_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_TOKEN_SECRET"]
+end
+client.update('wesh alors')
